128b170825fd456f5b33082ef14896addf7133ea
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-10-02 23:45:51 +0530

    feat: passport authentication for login

---
+++ controllers/passportConfig.js
@@ -0,0 +1,79 @@
+const passport = require("passport")
+const {PrismaClient} = require("../generated/prisma");
+const prisma = new PrismaClient();
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+exports.autoLogin = async(req,res,next) => {
+    const emailID = req.body.email;
+
+    const user = await prisma.user.findUnique({
+        where:{
+            email:emailID
+        }
+    })
+    console.log(user,'is the user guys!');
+
+    req.login(user, function(err){
+        if ( err ){
+            console.log('error!');
+            return next(err);
+        }
+        return res.redirect("/dashboard");
+    })
+}
+
+exports.autoLogout = async(req,res,next) =>{
+    req.logout(function(err){
+        if(err){
+            return next(err)
+        }
+        res.redirect("/");
+    })
+}
+
+
+passport.serializeUser((user,done) => {
+    console.log(user,'IS HERE');
+    done(null, user.id)
+})
+
+passport.deserializeUser(async(id, done)=>{
+    try{
+        const user = await prisma.user.findUnique({
+            where:{
+                id: id
+            }
+        })
+        console.log(user, "is query oppp");
+        done(null,user);
+    }
+    catch(err){
+        return done(err);
+    }
+})
+
+passport.use(
+    new LocalStrategy(async(username,password,done) => {
+        try{
+            const user = await prisma.user.findUnique({
+                where:{
+                    email: username
+                }
+            })
+            if (user === null){
+                throw new Error("Incorrect email");
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if (!match){
+                throw new Error("Passwords don't match");
+            }
+
+            return done(null,user);
+        }
+        catch(err){
+            done(err);
+        }
+    })
+)
