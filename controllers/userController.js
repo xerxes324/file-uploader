845c5aa2c2984ab0fa3cf12957eb1fbfe1080127
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-10-11 18:55:45 +0530

    add controller functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const folders = require("../controllers/folderManager")
+
 exports.login= async(req,res,next) =>{
     console.log("hety");
     res.render("login", {errors: ""});
@@ -8,6 +10,57 @@ exports.signup = async(req,res,next) => {
 }
 
 exports.dashboard = async(req,res,next) =>{
-    console.log(req.user,'is he');
-    res.render("dashboard", {user: req.user});
+    
+    const location = req.url;
+    console.log(location, 'Is the window loc');
+
+    if ( location === "/dashboard"){
+
+        const fd = await folders.getFoldersByID(req.user.id);
+        console.log(fd,'is the folders.');
+        if ( !fd || fd.length === 0){ //files in home dir
+            res.render("dashboard", {user:req.user, folders:[]})
+        }
+        else{ // if nothing in home directory
+            console.log("works.");
+            res.render('dashboard', {user: req.user, folders :fd})
+        }
+    }
+    else{
+        const parentfolderID = req.query.id;
+        const subfd = folders.getSubfoldersByID(parseInt(parentfolderID));
+        res.render("dashboard",{
+            user: req.user, 
+            folders: subfd
+        })
+    }
 }
+    
+
+exports.routeLocation = async(req,res,next)=>{
+    console.log(res.locals.location, 'is the loc in route');
+
+    if ( res.locals.location === "/dashboard"){
+        const fd = await folders.getFoldersByID(req.user.id);
+        console.log(fd,'is the folders.');
+        if ( !fd || fd.length === 0){ //files in home dir
+            res.render("dashboard", {user:req.user, folders:[]})
+        }
+        else{ // if nothing in home directory
+            console.log("works.");
+            res.render('dashboard', {user: req.user, folders :fd})
+        }
+    }
+
+    else{
+        const parentfolderID = req.query.id;
+        const subfd = folders.getSubfoldersByID(parseInt(parentfolderID));
+        res.render("dashboard",{
+            user: req.user, 
+            folders: subfd
+        })
+    }
+
+}
+
+// FIX REPETITIVE CODE FUNCTIONS LATER. 
