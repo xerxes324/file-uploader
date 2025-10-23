f59c5bdcb4640e29185225e507f405d0f94d4df9
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-10-24 00:08:08 +0530

    cleaned up code for readability

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -2,20 +2,6 @@ const {PrismaClient} = require("../generated/prisma");
 const prisma = new PrismaClient();
 const bcrypt = require('bcryptjs');
 
-
-// async function main(){
-//     const res = await prisma.user.findMany();
-//     console.log(res, "is the query o.p");
-// }
-
-// main().then(async()=>{
-//     await prisma.$disconnect()
-// }).catch(async(e)=>{
-//     console.error(e);
-//     await prisma.$disconnect()
-//     process.exit(1);
-// })
-
 exports.emailExists = async(req,res,next) =>{
     const exists = await prisma.users.findUnique({
         where:{
@@ -32,7 +18,6 @@ exports.emailExists = async(req,res,next) =>{
 
 exports.createUser = async(req,res,next) =>{
     const password = await bcrypt.hash(req.body.signuppassword,10);
-    console.log(password,'is the pwd');
     await prisma.users.create({
         data:{
             name:req.body.username,
@@ -40,15 +25,4 @@ exports.createUser = async(req,res,next) =>{
             password: password
         }
     })
-
-    //remove this after code completion
-    const users = await prisma.users.findMany();
-    console.log(users,'is prisma op');
-    next(); 
 }
-
-        // <% if (folders && folders.length > 0) { %>
-        // <% folders.forEach((value) => { %>
-        //     <h2><%= value.folder_name %></h2>
-        // <% }) %>
-        // <% } %>
