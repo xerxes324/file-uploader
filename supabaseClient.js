938dca82bab6ab92e698f40a06f06b49e7db3e45
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-10-23 23:59:47 +0530

    protected urls with .env for security

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,8 @@
-const {createClient} = require('@supabase/supabase-js')
-const supabase = createClient(
-    'https://vtoqlmnycrmhvyvjvovo.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0b3FsbW55Y3JtaHZ5dmp2b3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MjEyMjIsImV4cCI6MjA3NjA5NzIyMn0.HSwRYvHgRdxVDU36o29yrLrtoZ-dv7TpgbzkVVV9Rh4'
-)
+const { createClient } = require('@supabase/supabase-js');
 
-module.exports = {supabase};
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+module.exports = { supabase };
