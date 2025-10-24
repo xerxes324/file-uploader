f883089e0d20ab8fe5ca1625b4d78d438f33a46a
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-10-24 22:52:26 +0530

    feat : cleaned up code for readability

--- controllers/folderManager.js
+++ controllers/folderManager.js
@@ -1,18 +1,12 @@
 const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
-// req.url -> gives /newfolder 
-// req.body.location -> gives dahsboard here and /newfolder in controller
 
 exports.addFolder = async(req,res,next)=>{
-    console.log(req.url,"is the location");
-    console.log(req.user,"is the user");
 
     res.locals.location = req.body.location;
-    console.log(res.locals.location, 'iS the loc');
     if ( res.locals.location === "/dashboard"){
 
-        //set folders PARENTID to null;
-        
+        //set folders PARENT_ID to null
         await prisma.folders.create({
             data:{
                 folder_name: req.body.foldername,
@@ -23,36 +17,19 @@ exports.addFolder = async(req,res,next)=>{
     }
 
     else{
-
-        // set folders parentID to query value
-        // const folderID = req.query.id;
-        // console.log(req.url, 'is the subfolder URL');
-        console.log(req.session.parentFolderID, 'is the parent ID');
-        console.log(req.body, 'is the body');
-
         await prisma.folders.create({
             data:{
                 folder_name : req.body.foldername,
-                // parent_id: req.session.parentFolderID,
                 parent:{connect:{id: req.session.parentFolderID}},
                 user:{connect:{id: req.user.id}}  
             }
         })
-
-        console.log("PRISMA DONE");
     }
 
     next();
 }
 
-
-exports.addSubfolder = async(req,res,next) => {
-
-    console.log(req.query.id, 'is the ID');
-    next();
-}
-
-// runs when u click on a folder. 
+// runs when user clicks on a folder
 exports.displayFolderContents = async(req,res,next)=>{
     console.log(req.query.id, 'is the query id');
     const folderID = parseInt(req.query.id);
@@ -69,7 +46,6 @@ exports.getFoldersByID = async(id) =>{
     const folders = await prisma.folders.findMany({
         where : {user_id : id, parent_id: null}
     });
-    // console.log(folders,'is folder op');
     return folders;
 }
 
@@ -89,21 +65,5 @@ exports.getFiles = async(id) => {
     const files = await prisma.files.findMany({
         where:{folder_id : id}
     });
-    console.log(files,"is the files output prisma");
     return files;
 }
-
-
-
-
-
-/*
-
-Adding -> 
-if URL = /dashboard : prismacreate with folder parent = null
-else : prismacreate with folder parent = parentfolderID
-
-Disp -> get the URL from the frontend and if dash -> query accordingly 
-if not, then redir to dashboad/id ( define a get req for this )
-
-*/
