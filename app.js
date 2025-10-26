c0fc1445f8d375a8506bec99ba65cd137889358e
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-10-26 13:56:46 +0530

    added prisma session store for persisting sessions

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ const app = express();
 const router = require("./routes/userRouter");
 const passport = require("passport");
 const session = require('express-session');
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client/extension");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(session({
+
+    cookie:{
+        maxAge:7*24*60*60*1000
+    },
+
     secret: "keyboard cats",
     resave : false,
-    saveUninitialized : false
+    saveUninitialized : false,
+    store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    )
 }));
 
 app.use(passport.initialize());
